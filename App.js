d429e52cd8d5b72adecc2c64186b70b3875c360a
André Kitano <andre.kitano.silva@gmail.com>
2019-11-16 01:59:02 -0300

    Init baseUrl for good practices TODO// moving Enums to env

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Routes from './src/routes';
+import Service from './src/service/BaseAxios';
+import ServiceEnums from './src/service/BaseEnum';
 
 export default function App() {
+  Service.init(ServiceEnums.BASE_URL);
   return <Routes />
 }
 
