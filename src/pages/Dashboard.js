887e342da69bfd82bc4027a91ac07460070076ad
André Kitano <andre.kitano.silva@gmail.com>
2019-11-16 23:18:39 -0300

    Added options for using bottom navigation

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import {Platform, SafeAreaView, ScrollView, StatusBar, StyleSheet} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet, ToastAndroid, View} from 'react-native';
 
+import {Avatar, Header, Text} from 'react-native-elements';
 import Card from "../components/Card";
 
 import Service from '../service/BaseAxios';
@@ -17,11 +18,26 @@ export default function Dashboard({navigation}) {
         });
     }
 
+    const name = 'André Kitano';
     return (
         <SafeAreaView style={styles.container}>
+            <Header
+                containerStyle={{padding: 0}}
+                backgroundColor={'#fff'}
+                placement={"left"}
+                statusBarProps={{barStyle: 'dark-content'}}
+                leftContainerStyle={{marginStart: 16}}
+                leftComponent={<Avatar source={require('../../assets/avataaars.png')} size="small" rounded title="MT"
+                                       onPress={() => ToastAndroid.show(name, ToastAndroid.SHORT)}
+                                       activeOpacity={0.7}/>}
+                centerComponent={{text: "Olá " + name, style: {color: '#01755D', fontWeight: 'bold', fontSize: 16}}}/>
+            <View style={{backgroundColor: '#fff', padding: 0, marginTop: -10, width: '100%'}}>
+                <Text h4 style={{color: '#01755D', fontWeight: 'bold', fontSize: 12, marginStart: 22}}> Petições do
+                    dia</Text>
+            </View>
             <ScrollView>
                 {petitions.map((value, index) => {
-                    return <Card name={value.name} url={value.image} desc={value.desc}/>
+                    return <Card callback={() => alert("oi")} name={value.name} url={value.image} desc={value.desc}/>
                 })}
             </ScrollView>
         </SafeAreaView>
@@ -34,7 +50,6 @@ const styles = StyleSheet.create({
         height: '100%',
         justifyContent: 'space-between',
         alignItems: 'center',
-        backgroundColor: '#ecfcff',
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        backgroundColor: '#f2f4f6',
     },
 });
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,13 @@ import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
-import Dashboard from "./pages/Dashboard";
+import Options from "./pages/Options";
 
 const Routes = createAppContainer(
     createSwitchNavigator({
+        Options,
         Home,
-        Login,
-        Dashboard
+        Login
     })
 );
 
