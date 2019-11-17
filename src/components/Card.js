bb2be4eeb47faaa580e81e87e7eee33a14a86d83
André Kitano <andre.kitano.silva@gmail.com>
2019-11-16 23:17:56 -0300

    Updated card component and added bottomnavigation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import {StyleSheet, Text} from 'react-native'
-import {Card} from 'react-native-elements'
+import {StyleSheet, Text, TouchableOpacity} from 'react-native'
+import {Card} from 'react-native-elements';
 
 const CustomCard = (props) => {
     return (
-        <Card containerStyle={styles.container} title={props.name} image={{uri: props.url}}>
-            <Text style={{marginBottom: 10}}>
-                {props.desc}
-            </Text>
-        </Card>
+        <TouchableOpacity onPress={props.callback}>
+            <Card containerStyle={styles.container} title={props.name} image={{uri: props.url}}>
+                <Text style={{marginBottom: 10}}>
+                    {props.desc}
+                </Text>
+            </Card>
+        </TouchableOpacity>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        borderRadius: 10
+        borderRadius: 16
     },
 });
 
